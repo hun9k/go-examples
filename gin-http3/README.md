fae09ba569f62c688d43a0d97aa7040906fe449e
9k <540090808@qq.com>
2025-11-18 21:35:55 +0800

    add gin-http3 example

---
+++ gin-http3/README.md
@@ -0,0 +1,7 @@
+## gin-http3
+gin使用HTTP/3的例子
+
+```shell
+go mod tidy
+go run .
+```
---
+++ gin-http3/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log/slog"
+	"net/http"
+	"sync"
+
+	"github.com/gin-gonic/gin"
+	"github.com/quic-go/quic-go/http3"
+)
+
+func main() {
+	// 1. 初始化Handler（Gin 引擎）
+	handler := gin.Default()
+
+	// 2. 定义路由
+	handler.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello "+c.Request.Proto)
+	})
+
+	// 证书和密钥文件地址
+	certFile, keyFile := "localhost.pem", "localhost-key.pem"
+
+	// 同时启动多服务监听，使用wg管理goroutine
+	wg := &sync.WaitGroup{}
+
+	// 启动 HTTP/3 服务QUIC(TLS)
+	wg.Go(func() {
+		defer wg.Done()
+		if err := http3.ListenAndServeQUIC(":443", certFile, keyFile, handler); err != nil {
+			slog.Error(err.Error())
+		}
+	})
+
+	// 启动 HTTP/1.1/2 服务TCP+TLS
+	wg.Go(func() {
+		defer wg.Done()
+		if err := http.ListenAndServeTLS(":443", certFile, keyFile, handler); err != nil {
+			slog.Error(err.Error())
+		}
+	})
+
+	// 启动 HTTP/1.1/2 服务TCP
+	wg.Go(func() {
+		defer wg.Done()
+		if err := http.ListenAndServe(":80", handler); err != nil {
+			slog.Error(err.Error())
+		}
+	})
+
+	wg.Wait()
+}
